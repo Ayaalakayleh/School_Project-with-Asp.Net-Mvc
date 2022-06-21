ebb17173494e080470f4375cf2fb620967e0915e
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2022-06-21 20:28:38 +0300

    Add Student Model

---
+++ School_Project/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace School_Project.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}

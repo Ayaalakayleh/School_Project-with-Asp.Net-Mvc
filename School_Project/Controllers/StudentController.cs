dfbb36c07aa80d7a83250a8be70facc3f57f1522
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2022-06-21 21:30:33 +0300

    Add Student List View

--- School_Project/Controllers/StudentController.cs
+++ School_Project/Controllers/StudentController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using School_Project.Models;
 namespace School_Project.Controllers
 {
     public class StudentController : Controller
     {
+        static IList<Student> studentList = new List<Student>{
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+            };
+
         public ActionResult Index() // handles GET requests by default
         {
-            return View();
+            return View(studentList);
         }
 
     }

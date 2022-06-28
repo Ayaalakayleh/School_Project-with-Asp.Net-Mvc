7f28797f1744e1c1c0031b7f68c7564f3fdd3bde
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2022-06-28 15:09:26 +0300

    Add Edit/Create Student method and View

--- School_Project/Controllers/StudentController.cs
+++ School_Project/Controllers/StudentController.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using School_Project.Models;
+
 namespace School_Project.Controllers
 {
     public class StudentController : Controller
     {
-        
-
-        public ActionResult Index() 
-        {
-            Student[] studentList = {
+            static List<Student> studentList = new List<Student>(){
                 new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
                 new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
                 new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
@@ -16,9 +13,49 @@ namespace School_Project.Controllers
                 new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
                 new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
                 new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
-                };
+            };
+
+        public ActionResult Index() 
+        {
             return View(studentList);
         }
 
+        public ActionResult Edit(int? Id)
+        {
+            if (Id != null)
+            {
+                var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+                return View(std);
+            }
+            else
+            {
+                return View();
+            }  
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if (std.StudentId == 0)
+            {
+                std.StudentId = MaxId();
+                studentList.Add(std);                
+            }
+            else 
+            {             
+                var student = studentList.Where(s => s.StudentId == std.StudentId).FirstOrDefault();
+                studentList.Remove(student);
+                studentList.Add(std);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public int MaxId() 
+        {          
+            int maxid = studentList.Max(x => x.StudentId);
+            int studentID = maxid + 1;
+            return studentID;
+        }
     }
 }
